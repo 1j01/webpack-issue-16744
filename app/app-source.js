c8be2a53f40d3a352b237527788af3c679dd4216
Isaiah Odhner <isaiahodhner@gmail.com>
2023-02-19 20:33:11 -0500

    It also fails with import

--- app/app-source.js
+++ app/app-source.js
@@ -1,3 +1,6 @@
-const library = require("../library/dist/library-bundle.esm.js");
+// Either import or require fails
+import * as library from "../library/dist/library-bundle.esm.js";
+// const library = require("../library/dist/library-bundle.esm.js");
+
 console.log("library:", library);
 console.log("library.foo:", library.foo);
